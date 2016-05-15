0b2f64c0ac5dd73c829881c17d46fd5068db2f65
Svetlana <sgorishna@gmail.com>
2016-05-16 01:05:35 +0300

    The code has been formatted

--- JsLessons/js/Homework_2.js
+++ JsLessons/js/Homework_2.js
@@ -1,50 +1,47 @@
- function getIndexOfANumberInArray (array,number){
-	
-	 var index;
-	 
-	 for(var i =0; i<array.length; i++){
-		
-		if(array[i]==number){
+function getIndexOfANumberInArray(array, number) {
+
+	var index;
+
+	for (var i = 0; i < array.length; i++) {
+
+		if (array[i] == number) {
 			index = i;
 			break;
-	
-		}	else {
-			
+
+		} else {
+
 			index = -1;
-		} 
-	 }
-	 
-return index; 
-	
-}; 
-
-function arrayReverse (array){
-	
-	var reversed= [];
-	
-	for(var i =array.length-1; i>=0; i--){
-		
-		reversed.push(array[i]);
-		
-	}
-	
-	return reversed;
-	
-};
-
-
-function replaceEvenElementsInArray(array, replacement){
-	
-	for(var i =0; i<array.length; i++){
-		
-		if(array[i]%2 == 0){
-			
-			array[i] = replacement;
-			
 		}
 	}
-	
+
+	return index;
+
+};
+
+function arrayReverse(array) {
+
+	var reversed = [];
+
+	for (var i = array.length - 1; i >= 0; i--) {
+
+		reversed.push(array[i]);
+
+	}
+
+	return reversed;
+
+};
+
+function replaceEvenElementsInArray(array, replacement) {
+
+	for (var i = 0; i < array.length; i++) {
+
+		if (array[i] % 2 == 0) {
+
+			array[i] = replacement;
+
+		}
+	}
+
 	return array;
 }
-
-
